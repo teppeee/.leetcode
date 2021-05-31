18fc9db76011fe1f9bbfce6dee0a5d99223f27cf
teppeee <roundabout912@gmail.com>
2021-06-01 00:15:37 +0900

    11

--- 11.container-with-most-water.rs
+++ 11.container-with-most-water.rs
@@ -7,21 +7,24 @@
 // @lc code=start
 impl Solution {
     fn max_area(height: Vec<i32>) -> i32 {
+        let len = height.len();
         let mut i = 0;
-        let mut j = height.len() - 1;
+        let mut j = len - 1;
         let mut max = 0;
-    
-        while i < j {
-            let h = height[i].min(height[j]);
-            max = max.max(h * (i - j) as i32);
-    
-            if height[i] < height[j] {
-                i += 1;
-            } else {
-                j -= 1;
+        
+        loop {
+            if i >= j { break; }
+            let h_i = height[i];
+            let h_j = height[j];
+            let temp = std::cmp::min(h_i, h_j) * (j - i) as i32;
+            if max < temp {
+                max = temp;
             }
+            
+            if h_i < h_j { i += 1; }
+            else { j -= 1; }
         }
-    
+        
         max
     }
 }
