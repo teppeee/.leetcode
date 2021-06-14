9d0ec406a4d0274bf3a3cfa0a87d19df0e21c74e
teppeee <roundabout912@gmail.com>
2021-06-14 23:07:37 +0900

    626

---
+++ 626.exchange-seats.sql
@@ -0,0 +1,22 @@
+/* Write your T-SQL query statement below */
+SELECT
+    (
+        CASE
+            WHEN (id % 2) != 0
+            AND counts != id THEN id + 1
+            WHEN (id % 2) != 0
+            AND counts = id THEN id
+            ELSE id - 1
+        END
+    ) AS id,
+    student
+FROM
+    seat,
+    (
+        SELECT
+            COUNT(*) AS counts
+        FROM
+            seat
+    ) AS seat_counts
+ORDER BY
+    id ASC;
